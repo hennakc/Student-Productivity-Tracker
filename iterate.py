588c7c3475328edd37d0f510a2fabf21bb03987c
hennakc <hennaameen5@gmail.com>
2026-02-11 11:38:42 +0530

    update

--- iterate.py
+++ iterate.py
@@ -9,3 +9,21 @@ print(ite.__next__()) #show elements one by one.
 print(ite.__next__()) #second element shown.
 print(ite.__next__()) #third element shown.
 print(next(ite))
+
+#custom iteration:
+
+class Count:                   #use three functions to create an iterator.
+    def __init__(self):
+        self.num=1
+        self.max=0
+    def __iter__(self):
+        return self
+    def __next__():
+        if self.num<=self.max:
+            val=self.num
+            self.num+=1
+            return val
+        else:
+            raise StopIteration
+        
+ 
