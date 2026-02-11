ce56f8e8a2ceefb6c7190b03bd074c08889fa489
hennakc <hennaameen5@gmail.com>
2026-02-11 12:20:36 +0530

    Update lamda.py

--- lamda.py
+++ lamda.py
@@ -23,5 +23,10 @@ print(calc1())  #n number of variables and one expression.
 lst=[10,20,30,40]
 #output=[110,120,130,140]
 
-lst1=list(map(lambda x:x+100,lst))
+lst1=list(map(lambda x:x+100,lst)) #(lambda variables:expression,source)
 print(lst1)
+
+lst3=[10,13,14,17,16];
+lst4=list(filter(lambda x:x%2==0,lst3))
+print(lst3)
+print(lst4)
