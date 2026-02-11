a0fbc5c28183c3fb5aecd7ad1492396b71219874
hennakc <hennaameen5@gmail.com>
2026-02-11 12:21:37 +0530

    Update lamda.py

--- lamda.py
+++ lamda.py
@@ -26,6 +26,7 @@ lst=[10,20,30,40]
 lst1=list(map(lambda x:x+100,lst)) #(lambda variables:expression,source)
 print(lst1)
 
+#filters.
 lst3=[10,13,14,17,16];
 lst4=list(filter(lambda x:x%2==0,lst3))
 print(lst3)
